95572dd533e0877b5c912850ae7c0b79cb99f284
MD Ahasan Habib <xisan929@gmail.com>
2025-02-28 11:42:47 -0500

    Added Dompurify, jsDelivr CDN, Google analytics tag

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 alert("Welcome to my website!");
+function displayComment() {
+    // Get user input
+    const userInput = document.getElementById('userComment').value;
+
+    // Sanitize the input using DOMPurify
+    const cleanInput = DOMPurify.sanitize(userInput);
+
+    // Display the sanitized input
+    document.getElementById('commentDisplay').innerHTML = cleanInput;
+}
